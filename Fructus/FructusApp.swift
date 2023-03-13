0927cfa29f1d86668f3474b5644faee0cea1c585
Paulina Wyskiel <p.wyskiel@outlook.com>
2023-03-13 14:23:24 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Paulina Wyskiel on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
