d39bc8c5bea2d19963c8cc0bfcb9dbbbb8516e52
Paulina Wyskiel <p.wyskiel@outlook.com>
2023-03-14 17:26:41 +0100

    create ContentView and OnBoardingView first look and add more fruits

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -8,19 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - PROPERITIES
+    
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: -BODY
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView{
+            List {
+                ForEach(fruits.shuffled()) { fruit in
+                    FruitRowView(fruit: fruit)
+                        .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Fruits")
+        }//:NAVIGATION
     }
 }
 
+//MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
     }
 }
---
+++ Fructus/Model/FruitsModel.swift
@@ -0,0 +1,20 @@
+//
+//  FruitsModel.swift
+//  Fructus
+//
+//  Created by Paulina Wyskiel on 14/03/2023.
+//
+
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
